04f273c0063a0a3856e7b0fa00ef208deadf5e4c
massenandev <massenandev@gmail.com>
2021-10-01 13:19:24 -0300

    add methods

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -13,18 +13,23 @@ export class GamesRepository implements IGamesRepository {
   }
 
   async findByTitleContaining(param: string): Promise<Game[]> {
-    return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+    return  this.repository
+    .createQueryBuilder('games')
+    .select('games')
+    .where("LOWER(title) LIKE :title", { title:`%${param.toLowerCase()}%` })
+    .getMany() // Complete usando query builder
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query("SELECT COUNT(id) FROM GAMES") // Complete usando raw query
   }
 
   async findUsersByGameId(id: string): Promise<User[]> {
-    return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+    const games = await this.repository.createQueryBuilder("game")
+      .innerJoinAndSelect("game.users", "user")
+      .where("game.id = :id", { id })
+      .getOneOrFail()
+    
+    return games.users // Complete usando query builder
   }
 }
--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -1,4 +1,5 @@
-import { getRepository, Repository } from 'typeorm';
+import { relative } from 'path';
+import {  getRepository, Repository } from 'typeorm';
 
 import { IFindUserWithGamesDTO, IFindUserByFullNameDTO } from '../../dtos';
 import { User } from '../../entities/User';
@@ -11,20 +12,18 @@ export class UsersRepository implements IUsersRepository {
     this.repository = getRepository(User);
   }
 
-  async findUserWithGamesById({
-    user_id,
-  }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+  async findUserWithGamesById({ user_id }: IFindUserWithGamesDTO): Promise<User> {
+    return this.repository.createQueryBuilder('user')
+    .leftJoinAndSelect("user.games", "game")
+    .where("user.id = :id", { id: user_id })
+    .getOneOrFail();
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query("SELECT * FROM users ORDER BY first_name"); // Complete usando raw query
   }
 
-  async findUserByFullName({
-    first_name,
-    last_name,
-  }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+  async findUserByFullName({ first_name, last_name }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
+    return this.repository.query('SELECT * FROM users WHERE LOWER(first_name) = $1 AND LOWER(last_name) = $2', [first_name.toLowerCase(), last_name.toLowerCase()]);
   }
 }
